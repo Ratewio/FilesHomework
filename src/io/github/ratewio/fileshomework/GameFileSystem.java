10b5fca70d5e1ec6c71b9447bd83ac481527f491
Ratewio <ratewio@gmail.com>
2026-05-22 09:33:29 +0500

    all tree task completed

--- src/io/github/ratewio/fileshomework/GameFileSystem.java
+++ src/io/github/ratewio/fileshomework/GameFileSystem.java
@@ -1,7 +1,9 @@
 package io.github.ratewio.fileshomework;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class GameFileSystem {
@@ -22,9 +24,6 @@ public class GameFileSystem {
     }
 
     public GameFileSystem(File mainDir) {
-
-        File parent = mainDir.getParentFile();
-
         logger = new Logger("GameFileSystem");
 
         this.mainDir = mainDir;
@@ -32,7 +31,7 @@ public class GameFileSystem {
         logger.log("main dir assigned as " + mainDir.getPath());
     }
 
-    //Create directory tree and log
+    // Создаёт директорию игры и всё дерево директорий, сохраняет лог.
     public void init() {
         handleMkdir(getMainDir());
         handleMkdir(getSrc());
@@ -41,9 +40,11 @@ public class GameFileSystem {
         handleMkdir(getRes());
         handleMkdir(getSavegames());
         handleMkdir(getTemp());
-        logger.saveToFile(getTempFile());
+        writeLog();
     }
 
+
+    //вспомогательный метод логирующий создание директорий.
     private void handleMkdir(File file) {
         logger.log(
                 (file.exists()
@@ -91,24 +92,34 @@ public class GameFileSystem {
         logger.log("saving progress to " + file.getPath() + "...");
         try (ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)))) {
             oos.writeObject(gameProgress);
-            logger.log("progress saved to " + file.getPath());
         } catch (IOException e) {
             logger.log("cant save progress due exception: " + e);
         }
     }
 
-    public void zipFiles(File zipFile, File... files){
+    public GameProgress openProgress(File file) {
+        logger.log("loading progress from " + file.getPath() + "...");
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
+            GameProgress gameProgress = (GameProgress) ois.readObject();
+            return gameProgress;
+        } catch (IOException | ClassNotFoundException e) {
+            logger.log("cant load progress due exception: " + e);
+        }
+        return null;
+    }
+
+    public void zipFiles(File zipFile, File... files) {
         logger.log("zip files to " + zipFile.getPath() + "...");
-        try(ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)))){
-            for(File file : files){
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)))) {
+            for (File file : files) {
                 logger.log("zipping " + file.getPath() + "...");
                 ZipEntry entry = new ZipEntry(file.getName());
                 zos.putNextEntry(entry);
 
-                int bufferSize = 8192;
-                try(BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file), bufferSize)){
+                int bufferSize = 8192; //default from BufferedInputStream.DEFAULT_BUFFER_SIZE
+                try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file), bufferSize)) {
                     byte[] buffer = new byte[bufferSize];
-                    for(int len = 0; (len = bis.read(buffer)) != -1;){
+                    for (int len; (len = bis.read(buffer)) != -1; ) {
                         zos.write(buffer, 0, len);
                     }
                 }
@@ -116,13 +127,49 @@ public class GameFileSystem {
                 zos.closeEntry();
             }
             logger.log("zipped to " + zipFile.getPath());
+
+            for (File file : files) {
+                if (file.delete()) {
+                    logger.log("removed old file: " + file.getPath());
+                }
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        for(File file : files){
-            logger.log("removed old file: " + file.getPath());
-            file.delete();
+    public void unzipFiles(File zipFile, File dir) {
+        logger.log("unzip files to " + dir + "...");
+        int bufferSize = 8192; //default from BufferedInputStream.DEFAULT_BUFFER_SIZE
+        try (ZipInputStream zos = new ZipInputStream(new BufferedInputStream(new FileInputStream(zipFile), 8192))) {
+            byte[] buffer = new byte[bufferSize];
+
+            ZipEntry entry;
+            while ((entry = zos.getNextEntry()) != null) {
+                File file = new File(dir, entry.getName());
+                logger.log("unzipping " + file.getPath() + "...");
+                try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file))) {
+                    for (int len; (len = zos.read(buffer)) != -1; ) {
+                        bos.write(buffer, 0, len);
+                    }
+                }
+                zos.closeEntry();
+            }
+            logger.log("unzipped " + zipFile.getPath());
+
+            zos.close();
+
+            if (zipFile.delete()) {
+                logger.log("removed zip: " + zipFile.getPath());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public void writeLog() {
+        logger.saveToFile(getTempFile());
     }
 }
--- src/io/github/ratewio/fileshomework/Main.java
+++ src/io/github/ratewio/fileshomework/Main.java
@@ -6,12 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         GameFileSystem gfs = new GameFileSystem();
+        //инициализируем директорию игры, дерево папок и пишем логи в temp\temp.txt
         gfs.init();
 
         GameProgress Antony = new GameProgress(100, 2, 12, 1004.56);
         GameProgress Josh = new GameProgress(79, 1, 24, 950.23);
         GameProgress Jeff = new GameProgress(50, 4, 16, 869.12);
 
+        //Создаём файлы сохранений, чтобы потом не создавать снова при загрузке с диска. Сохраняем игровой прогресс
         File save1 = new File(gfs.getSavegames(), "save1.dat");
         File save2 = new File(gfs.getSavegames(), "save2.dat");
         File save3 = new File(gfs.getSavegames(), "save3.dat");
@@ -20,5 +22,16 @@ public class Main {
         gfs.saveGame(save3, Jeff);
 
         gfs.zipFiles(new File(gfs.getSavegames(), "zip.zip"), save1, save2, save3);
+
+
+        // Задание с *
+        gfs.unzipFiles(new File(gfs.getSavegames(), "zip.zip"), gfs.getSavegames());
+        //Выводим в консоль какие значения игрового прогресса были загружены с диска
+        System.out.println("(*) прогресс Antony: " + gfs.openProgress(save1).toString());
+        System.out.println("(*) прогресс Josh: " + gfs.openProgress(save2).toString());
+        System.out.println("(*) прогресс Jeff: " + gfs.openProgress(save3).toString());
+
+        // т.к последняя автоматическая запись была только на методе init() можно записать логи в temp.txt вызвав метод вручнуе если необходимо
+        // gfs.writeLog();
     }
 }
